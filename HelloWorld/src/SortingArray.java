cd4f045de9abf80943d7f5092dbf15ad4e404c83
mmz <User1@User2-PC>
2015-07-01 15:40:19 +0100

    added more details

--- HelloWorld/src/SortingArray.java
+++ HelloWorld/src/SortingArray.java
@@ -6,12 +6,17 @@
  */
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class SortingArray {
 
 	public static void main(String[] args){
+		int number =0;
+		Scanner numScan = new Scanner(System.in);
+		System.out.println("please enter a number:");
+		number = numScan.nextInt();
 		
-		int [] numArray = new int [10]; //create an array of 10 elements 
+		int [] numArray = new int [number]; //create an array of 10 elements 
 		Random random = new Random();
 		int randomNum =random.nextInt();
 		float numTotal=0;
@@ -47,5 +52,30 @@ public class SortingArray {
 		System.out.println("Maximum  is: "+ maxNum);
 		System.out.println("Minimum  is: "+ minNum);
 		System.out.println("Average  is: "+ verageNum);
+		
+		 numArray=Arrays.copyOf(numArray, 40);
+		 System.out.print("\nRandom Numbers");
+		 	for(int i=10; i<numArray.length;i++){
+		 		numArray[i]=random.nextInt(20+3);
+		 	}
+			for(int i=0; i< numArray.length; i++ ){
+				
+				//numArray[i]= random.nextInt(20+1); // assign random values to the array
+				
+				System.out.print(" "+numArray[i]);
+			}
+			int index=0;
+			Scanner indexScan = new Scanner(System.in);
+			System.out.print("please enter the number to search "+index);
+			index = indexScan.nextInt();
+			
+			 index = Arrays.binarySearch(numArray,11 ); // will search the number  in the array
+			if(index <0){
+				System.out.println("\nsorry the number is not listed");
+				
+			}else {
+				System.out.println("\nnumbers @ "+index);
+			}
+			
 	}
 }
