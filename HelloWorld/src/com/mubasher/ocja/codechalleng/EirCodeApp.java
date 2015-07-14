d019f487a1ea4722c6c435449eccf1b5fd890fa5
mmz <User1@User2-PC>
2015-07-14 15:22:30 +0100

    eircode app suing methods

--- HelloWorld/src/com/mubasher/ocja/codechalleng/EirCodeApp.java
+++ HelloWorld/src/com/mubasher/ocja/codechalleng/EirCodeApp.java
@@ -18,22 +18,19 @@ public class EirCodeApp {
 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int numAddres =7 ;
 
-		//		String [] addressArray = new String[numAddres];
+
 		String [] addressArray = {"5 Merrion Square North, Dublin 2", "10 Burlington Road, Dublin 4", 
 				"Dunsink Observatory, Dunsink Lane, Dublin 15", "26 KINCORA ROAD, Clontarf, Dublin 3", 
 				"Partas, 4A BROOKFIELD ENTERPRISE CENTRE, Dublin 24", "HODGES FIGGIS, 56-58 DAWSON STREET, Dublin 2", 
-		"CENTRAL BANK OF IRELAND, DAME STREET, Dublin 2", "", "", ""};		
-		//		String [] eirCodeArray = new String[numAddres];
+				"CENTRAL BANK OF IRELAND, DAME STREET, Dublin 2", "", "", ""};		
 		String [] eirCodeArray = {"D02Y006", "D04C932","D15XR2R","D03RR27","D24H510", "D02XE81", "D02P656", "", "", ""};
 		int customAddressRemaining = 3;
 		boolean programRunning= true;
 
 		do{
 			Scanner sc = new Scanner(System.in);
-			System.out.println("1) Find an address \n2) Find all adresses in an area \n3) Find an eircode"
+			System.out.println("1) Find an address \n2) Find all addresses in an area \n3) Find an eircode"
 					+ "\n4) Input an eircode and address\n5) Quit ");
 			int menuChoice = sc.nextInt();
 			switch (menuChoice){
@@ -45,13 +42,8 @@ public class EirCodeApp {
 					Scanner scan = new Scanner(System.in);
 					String userInput = scan.nextLine();
 					userInput=userInput.replace(" ", "");
-//					userInput=(userInput.substring(0, 3) + " " + userInput.substring(3));
-
-					//String userInput = "D15 XR2R";
-					//			System.out.println(userInput);
 
 					for(int i=0; i<eirCodeArray.length; i++){
-						//	if(userInput==eirCodeArray[i]){
 						if(userInput.equalsIgnoreCase(eirCodeArray[i])){
 							System.out.println("the address for the "+userInput +" is: "+addressArray[i]);
 							isValidInput = true;
@@ -82,22 +74,14 @@ public class EirCodeApp {
 					if (addressArray[i].toLowerCase().contains(case3Input.toLowerCase())){
 						System.out.println(eirCodeArray[i]);
 						System.out.println(addressArray[i]);
-						
+
 					}
 				}
 				break;
 				// search address
 
 			case 4:
-				/*5) Provide a facility for a user to enter up to 3 customs Eircode & Address combinations
-				 * 0) tell the user how many more times they can enter data (0-3). if 0, return to main menu
-				 * a) prompt user to enter code
-				 * b) change code to correct format (remove spaces and toUpper)
-				 * c) save this to array
-				 * d) prompt user to enter the address (single line with commas separating)
-				 * e) save this to array
-				 * f) increase the array index by 1 for the next time they are using this feature
-				 */ 
+
 				if (customAddressRemaining>0){
 					System.out.println("You may enter up to " + customAddressRemaining + " addresses and Eircodes");
 					System.out.println("Please enter the eircode: ");
@@ -109,7 +93,7 @@ public class EirCodeApp {
 					System.out.println("Please enter the address ");
 					case4Input = scan.nextLine();
 					addressArray[addressArray.length - customAddressRemaining ] = case4Input;
-					
+
 					System.out.println("the address you entered is : "+addressArray[addressArray.length - customAddressRemaining ] );
 					System.out.println("and the Eircode you entered is : " + eirCodeArray[eirCodeArray.length - customAddressRemaining]);
 					customAddressRemaining--;
@@ -117,8 +101,8 @@ public class EirCodeApp {
 					System.out.println("You have no custom addresses remaining");
 				}
 				break;
-				
-				
+
+
 			case 5:
 				System.out.println("Thank you, goodbye!");
 				programRunning = false; 
